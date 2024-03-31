9cba8605d20e71a05a2b1187154ef220349a10cf
yusuke_yokota <yusuke@lotusandcompanyinc.com>
2024-03-31 16:12:24 +0900

    Update tool

--- core/tool/tool.cpp
+++ core/tool/tool.cpp
@@ -1,5 +1,45 @@
 #include "tool.h"
+#include <QEvent>
+#include "core/exception/invalid_argument_exception.h"
+#include "core/exception/under_development_exception.h"
 
-Tool::Tool(Tool::ID id)
-    : id(id)
+const QString Tool::invalidToolIDReason
+    = QString("Tool::ID must be in range (%1, %2)").arg(Tool::ID_UNDEFINED).arg(Tool::ID_MAX);
+
+Tool::Tool(Tool::ID id, const QString &stringID, QObject *parent)
+    : QObject(parent)
+    , id(id)
+    , stringID(stringID)
+    , _info(info(id))
 {}
+
+void Tool::validateID(ID id)
+{
+    const int intID = static_cast<int>(id);
+
+    if (intID <= ID_UNDEFINED || ID_MAX <= intID)
+        throw InvalidArgumentException(intID, invalidToolIDReason);
+}
+
+const Tool::Info Tool::info(ID id)
+{
+    validateID(id);
+
+    // TODO: return Info{tr("Tool Name"), tr("Tool description")}
+    switch (id) {
+    default:
+        throw UnderDevelopmentException();
+    }
+}
+
+bool Tool::event(QEvent *event)
+{
+    switch (event->type()) {
+    case QEvent::LanguageChange:
+        QObject::event(event);
+        _info = info(id);
+        return true;
+    default:
+        return QObject::event(event);
+    }
+}
--- core/tool/tool.h
+++ core/tool/tool.h
@@ -10,24 +10,44 @@ class Tool : public QObject
     Q_OBJECT
 
 public:
+    Tool() = delete;
+    Tool(const Tool &) = delete;
+
     enum class ID {
         UNDEFINED,
         TOOL_ID_FIELDS, // ここに展開する
         // WIP
         MAX,
     };
+    const ID id;
+    const QString stringID;
+
+    // 翻訳が必要な情報
+    struct Info
+    {
+        QString name;
+        QString description;
+    };
 
     constexpr static const int ID_UNDEFINED = static_cast<int>(ID::UNDEFINED);
     constexpr static const int ID_MAX = static_cast<int>(ID::MAX);
 
-    const ID id;
-
-    virtual QString name() const = 0;
-    virtual QString description() const = 0;
+    static const Info info(ID id) noexcept(false);
+    const QString &toolName() const { return _info.name; }
+    const QString &toolDesctiption() const { return _info.description; }
 
 protected:
-    explicit Tool(ID id);
+    explicit Tool(ID id, const QString &stringID, QObject *parent = nullptr);
     virtual ~Tool() = default;
+
+    static const QString invalidToolIDReason;
+
+    static void validateID(ID id) noexcept(false);
+
+    virtual bool event(QEvent *event) override;
+
+private:
+    Info _info;
 };
 
 #endif // TOOL_H
