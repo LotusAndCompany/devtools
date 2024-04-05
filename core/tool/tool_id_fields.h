9354e2744968a29a8c769185f13d447fa4e7d0b4
yusuke_yokota <yusuke@lotusandcompanyinc.com>
2024-04-05 17:47:03 +0900

    Disable clang-format for `TOOL_ID_FIELDS()`

--- core/tool/tool_id_fields.h
+++ core/tool/tool_id_fields.h
@@ -1,10 +1,22 @@
 #ifndef TOOL_ID_FIELDS_H
 #define TOOL_ID_FIELDS_H
 
-// NOTE: 複数行にしたかったが、フォーマットすると纏まってしまう
-#define TOOL_ID_FIELDS() \
-    SAMPLE_0, SAMPLE_1, SAMPLE_2, SAMPLE_3, HTTP_REQUEST, COMMAND_GENERATION, IMAGE_RESIZE, \
-        IMAGE_ROTATION, IMAGE_SPLIT, IMAGE_TRANSPARENT, IMAGE_TRIM, IMAGE_COMBINE, \
-        PHRASE_GENERATION
+// NOTE: コード整形をこの部分だけ無効化する
+// clang-format off
+#define TOOL_ID_FIELDS()    \
+    SAMPLE_0,               \
+    SAMPLE_1,               \
+    SAMPLE_2,               \
+    SAMPLE_3,               \
+    HTTP_REQUEST,           \
+    COMMAND_GENERATION,     \
+    IMAGE_RESIZE,           \
+    IMAGE_ROTATION,         \
+    IMAGE_SPLIT,            \
+    IMAGE_TRANSPARENT,      \
+    IMAGE_TRIM,             \
+    IMAGE_COMBINE,          \
+    PHRASE_GENERATION
+// clang-format on
 
 #endif // TOOL_ID_FIELDS_H
