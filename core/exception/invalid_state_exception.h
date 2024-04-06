f985287ea52f7790603c3655cdd0743a32ea19e0
yusuke_yokota <yusuke@lotusandcompanyinc.com>
2024-04-06 17:31:24 +0900

    Add documentation for core/exception/invalid_state_exception.h

--- core/exception/invalid_state_exception.h
+++ core/exception/invalid_state_exception.h
@@ -9,24 +9,57 @@ class TestInvalidStateException;
 }
 #endif
 
+/**
+ * @brief 不正な状態になった時に発生させる例外
+ * @details これを発生させれば良いという事ではなく、これが発生するようなコードは直すべき
+ */
 class InvalidStateException : public CommonException
 {
 public:
+    /**
+     * @brief デフォルトコンストラクタ
+     */
     InvalidStateException() = default;
+    /**
+     * @brief コピーコンストラクタ
+     * @param コピー元インスタンス
+     */
     InvalidStateException(const InvalidStateException &src) = default;
+    /**
+     * @brief 任意のメッセージを設定できるコンストラクタ
+     * @param message メッセージ
+     */
     explicit InvalidStateException(const QString &message)
         : CommonException(message)
     {}
+    /**
+     * @brief その状態と理由からメッセージを生成するコンストラクタ
+     * @param actual その引数
+     * @param expected 不正な理由、正しい状態など
+     * 
+     * @details "[InvalidStateException] actual: <actual>, expected: <expected>" の形式のメッセージが設定される
+     */
     InvalidStateException(const QString &actual, const QString &expected)
     {
         message = QString("[InvalidStateException] actual: ") % actual % QString(", expected: ")
                   % expected;
     }
 
+    /**
+     * @brief このインスタンスを投げる
+     * 
+     * @sa [QException::raise()](https://doc.qt.io/qt-6/qexception.html#raise)
+     */
     virtual void raise() const override { throw *this; }
 
 protected:
-    // NOTE: protectedにしたい
+    /**
+     * @brief このインスタンスをコピーする
+     * @return コピーされたインスタンス
+     * @note この関数で生成されたインスタンスは`delete`等、何らかの方法で解放する必要がある
+     * 
+     * @sa [QException::clone()](https://doc.qt.io/qt-6/qexception.html#clone)
+     */
     virtual QException *clone() const override { return new InvalidStateException(*this); }
 
 #ifdef _TEST_InvalidStateException
