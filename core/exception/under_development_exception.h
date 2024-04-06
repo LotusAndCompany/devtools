c0b59bb69f8d1325316cd7da5294216b751d36dc
yusuke_yokota <yusuke@lotusandcompanyinc.com>
2024-04-06 17:44:30 +0900

    Add documentation for core/exception/under_development_exception.h

--- core/exception/under_development_exception.h
+++ core/exception/under_development_exception.h
@@ -9,14 +9,29 @@ class TestUnderDevelopmentException;
 }
 #endif
 
-// NOTE: 開発中以外は使わないように
+/**
+ * @brief 未実装の部分がある場合に発生させる例外
+ * @details 開発中に仕方無く発生させるものであって、リリース時にはこの例外が発生しないようにする
+ */
 class UnderDevelopmentException : public CommonException
 {
 public:
+    /**
+     * @brief デフォルトコンストラクタ
+     * @details "[UnderDevelopmentException] Under development..."というメッセージが設定される
+     */
     UnderDevelopmentException()
         : CommonException("[UnderDevelopmentException] Under development...")
     {}
+    /**
+     * @brief コピーコンストラクタ
+     * @param src コピー元インスタンス
+     */
     UnderDevelopmentException(const UnderDevelopmentException &src) = default;
+    /**
+     * @brief 任意のメッセージを設定できるコンストラクタ
+     * @param message メッセージ
+     */
     explicit UnderDevelopmentException(const QString &message)
         : CommonException(message)
     {}
@@ -24,7 +39,6 @@ public:
     virtual void raise() const override { throw *this; }
 
 protected:
-    // NOTE: protectedにしたい
     virtual QException *clone() const override { return new UnderDevelopmentException(*this); }
 
 #ifdef _TEST_UnderDevelopmentException
