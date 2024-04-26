dc91c35f9c7d9c88ff182374d422bd80f1f7b78b
yusuke_yokota <yusuke@lotusandcompanyinc.com>
2024-04-26 19:05:40 +0900

    Refactor FunctionMock Now FunctionMock supports arguments history

--- tests/mock_helper.h
+++ tests/mock_helper.h
@@ -4,11 +4,107 @@
 /// mockクラスの実装に使える、内容を差し替えられる関数オブジェクトっぽいものを定義する
 
 #include <functional>
+#include <tuple>
 #include <type_traits>
+#include <vector>
 
 namespace Test {
 /**
- * @brief 基底クラス
+ * @brief 対象の型のデフォルト値を取得する
+ * @tparam result_type 関数の返り値の型
+ */
+template<typename result_type>
+struct _DefaultValue
+{
+    /**
+     * @brief デフォルト値を返す
+     * @return デフォルト値
+     */
+    inline static result_type get()
+    {
+        if constexpr (std::is_void<result_type>::value) {
+            // void
+            return;
+        } else if constexpr (std::is_pointer<result_type>::value) {
+            // pointer
+            return nullptr;
+        } else if constexpr (std::is_fundamental<result_type>::value) {
+            // primitive types such as int, float, char, bool, etc...
+            return static_cast<result_type>(0);
+        } else {
+            // other types except reference. default constructor required.
+            return result_type();
+        }
+    }
+};
+
+/**
+ * @brief 参照型の場合の特殊化
+ */
+template<typename result_type>
+struct _DefaultValue<result_type &>
+{
+    /**
+     * @brief デフォルト値の参照を返す
+     * @return デフォルト値の参照
+     */
+    inline static result_type &get() { return value; }
+
+private:
+    /// デフォルト値(デフォルトコンストラクタが必要)
+    static result_type value;
+};
+
+template<typename T>
+T _DefaultValue<T &>::value;
+
+/**
+ * @brief 渡された引数を記憶していく関数オブジェクト。引数が複数のバージョン。
+ * @tparam arg_type 引数の型
+ */
+template<typename... arg_types>
+struct _ArgumentsHistory
+{
+    using element_type = std::tuple<arg_types...>;
+
+    /**
+     * @brief 初期化する
+     */
+    inline void clear() { history.clear(); }
+    /**
+     * @brief 指定のインデックスの履歴を返す
+     * @param index インデックス。0=最古、size()-1または-1=最新。
+     * @return 引数の履歴
+     */
+    inline const element_type &at(int index) const
+    {
+        if (index < 0)
+            return history.at(history.size() + index);
+        else
+            return history.at(index);
+    }
+    /**
+     * @brief 要素を追加する
+     * @param args 引数
+     */
+    inline void push_back(const element_type &args) { history.push_back(args); }
+
+private:
+    /// 引数の履歴
+    std::vector<element_type> history;
+};
+
+/**
+ * @brief void型用の特殊化
+ */
+template<>
+struct _ArgumentsHistory<void>
+{
+    using element_type = void;
+};
+
+/**
+ * @brief mockの実装に使える、中身を入れ替えられる関数オブジェクト
  * @tparam result_type 関数の返り値の型
  * @tparam arg_types 関数の引数の型
  * @details std::function<return_type(arg_types...)>型を受け取れる。@n
@@ -35,14 +131,26 @@ namespace Test {
  * isGraterThan0(-10) == true;
  * @endcode
  */
-template<typename result_type, typename... arg_types>
-class _FunctionMockBase
+template<typename result_type = void, typename... arg_types>
+class FunctionMock
 {
     using function_type = std::function<result_type(arg_types...)>;
+    using history_type = typename _ArgumentsHistory<arg_types...>::element_type;
 
 public:
     /**
-     * @brief func を呼び出し _count を一つ増やす
+     * @brief デフォルトコンストラクタ
+     */
+    FunctionMock() {}
+    /**
+     * @brief func を設定するコンストラクタ
+     * @param f 設定する関数
+     */
+    FunctionMock(const function_type &f)
+        : func(f)
+    {}
+    /**
+     * @brief func を呼び出し _count を一つ増やす。引数があれば argsHistory に追加する。
      * @param args 引数
      * @return func が設定されていれば func の返り値、設定されていなければデフォルトの値。
      */
@@ -50,10 +158,14 @@ public:
     {
         _count++;
 
+        if constexpr (!std::is_void<history_type>::value) {
+            argsHistory.push_back({args...});
+        }
+
         if (hasFunction()) {
             return func(args...);
         } else {
-            return defaultBehavior(args...);
+            return _DefaultValue<result_type>::get();
         }
     }
 
@@ -78,116 +190,60 @@ public:
      * @brief この関数オブジェクトが呼び出された回数を返す
      * @return 呼び出された回数
      */
-    inline int count() const { return _count; }
+    inline size_t count() const { return _count; }
     /**
      * @brief 呼び出された回数を0に戻す
      */
-    inline void resetCount() { _count = 0; }
+    void resetCount()
+    {
+        _count = 0;
+        if constexpr (!std::is_void<history_type>::value) {
+            argsHistory.clear();
+        }
+    }
 
     /**
      * @brief func を設定する
-     * @param keepCount `true`なら _count を初期化しない
+     * @param keepCount `true`なら _count と argsHistory とを初期化しない
      */
     void setFunction(const function_type &f, bool keepCount = false)
     {
         if (!keepCount)
             resetCount();
+
         func = f;
     }
 
     /**
      * @brief func を削除する
-     * @param keepCount `true`なら _count を初期化しない
+     * @param keepCount `true`なら _count と argsHistory とを初期化しない
      */
     inline void clearFunction(bool keepCount = false) { setFunction(function_type(), keepCount); }
 
-protected:
     /**
-     * @brief デフォルトコンストラクタ
+     * @brief この関数オブジェクトに渡された引数の履歴を返す
+     * @param index インデックス。0=最古、size()-1または-1=最新。
+     * @return 渡された引数の履歴
      */
-    _FunctionMockBase() {}
-    /**
-     * @brief func を設定するコンストラクタ
-     * @param f 設定する関数
-     */
-    _FunctionMockBase(const function_type &f)
-        : func(f)
-    {}
+    inline const history_type &argumentsHistory(int index = -1) const
+    {
+        if constexpr (std::is_void<history_type>::value) {
+            // void
+            return;
+        } else {
+            return argsHistory.at(index);
+        }
+    }
 
+private:
     /// invoke() で呼ばれる関数
     function_type func;
 
-    /**
-     * @brief func が設定されていない時に呼ばれる処理。デフォルトの挙動。
-     * @param args 引数
-     * @return デフォルトの返り値
-     */
-    virtual result_type defaultBehavior(arg_types... args) = 0;
-
-private:
     /// この関数オブジェクトが呼び出された回数
-    int _count = 0;
-};
+    size_t _count = 0;
 
-/**
- * @brief 多くの場合に使える_FunctionMockBaseの実装
- */
-template<typename result_type = void, typename... arg_types>
-class FunctionMock : public _FunctionMockBase<result_type, arg_types...>
-{
-public:
-    using function_type = std::function<result_type(arg_types...)>;
-
-    FunctionMock()
-        : _FunctionMockBase<result_type, arg_types...>()
-    {}
-    FunctionMock(const function_type &f)
-        : _FunctionMockBase<result_type, arg_types...>(f)
-    {}
-
-protected:
-    result_type defaultBehavior(arg_types... args) override
-    {
-        if constexpr (std::is_void<result_type>::value) {
-            // void
-            return;
-        } else if constexpr (std::is_pointer<result_type>::value) {
-            // pointer
-            return nullptr;
-        } else if constexpr (std::is_fundamental<result_type>::value) {
-            // primitive types such as int, float, char, bool, etc...
-            return static_cast<result_type>(0);
-        } else {
-            // other types except reference. default constructor required.
-            return result_type();
-        }
-    }
-};
-
-/**
- * @brief 返り値が参照型の時のFunctionMockの特殊化
- */
-template<typename result_type, typename... arg_types>
-class FunctionMock<result_type &, arg_types...>
-    : public _FunctionMockBase<result_type &, arg_types...>
-{
-public:
-    using function_type = std::function<result_type &(arg_types...)>;
-
-    FunctionMock()
-        : _FunctionMockBase<result_type &, arg_types...>()
-    {}
-    FunctionMock(const function_type &f)
-        : _FunctionMockBase<result_type &, arg_types...>(f)
-    {}
-
-protected:
-    result_type &defaultBehavior(arg_types... args) override { return defaultReturnValue; }
-
-private:
-    // default constructor required
-    /// デフォルトの返り値
-    result_type defaultReturnValue;
+    /// この関数オブジェクトに渡された引数の履歴
+    _ArgumentsHistory<arg_types...> argsHistory;
 };
 }
 
