0b429c8ed4841c15736ae5037e7bc367fdfa2c88
yusuke_yokota <yusuke@lotusandcompanyinc.com>
2024-10-28 09:52:15 +0900

    Update CMakeLists.txt Use qt_add_library over qt_add_executable to compile time saving

--- res/dev-tools_en.ts
+++ res/dev-tools_en.ts
@@ -1,4 +1,355 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1">
+<context>
+    <name>AboutDevToolsDialog</name>
+    <message>
+        <location filename="../gui/menubar/about_devtools_dialog.ui" line="14"/>
+        <source>About DevTools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/menubar/about_devtools_dialog.ui" line="24"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/menubar/about_devtools_dialog.ui" line="121"/>
+        <source>Qt Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/menubar/about_devtools_dialog.ui" line="167"/>
+        <source>Application Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/menubar/about_devtools_dialog.ui" line="190"/>
+        <source>Build Environment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/menubar/about_devtools_dialog.ui" line="203"/>
+        <source>Revision</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/menubar/about_devtools_dialog.ui" line="265"/>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/menubar/about_devtools_dialog.ui" line="288"/>
+        <source>Thirdparty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/menubar/about_devtools_dialog.ui" line="305"/>
+        <source>DevTools relies on the following thirdparty softwares</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/menubar/about_devtools_dialog.ui" line="330"/>
+        <source>TODO: Grid Layoutにする、リンクを追加する</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/menubar/about_devtools_dialog.ui" line="382"/>
+        <source>License</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/menubar/about_devtools_dialog.ui" line="394"/>
+        <source>No contents, Work in progress</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BasicImageView</name>
+    <message>
+        <location filename="../gui/image/basic/image_view.ui" line="86"/>
+        <location filename="../gui/image/basic/image_view.cpp" line="125"/>
+        <source>No Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/basic/image_view.ui" line="142"/>
+        <source>Zoom In</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/basic/image_view.ui" line="190"/>
+        <source>Zoom Out</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BasicImageViewControl</name>
+    <message>
+        <location filename="../gui/image/basic/control.ui" line="20"/>
+        <source>Load</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/basic/control.ui" line="27"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/basic/control.ui" line="34"/>
+        <location filename="../gui/image/basic/control.cpp" line="53"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ImageDivisionGUI</name>
+    <message>
+        <location filename="../gui/image/division/image_division_gui.ui" line="85"/>
+        <source>Image size:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/division/image_division_gui.ui" line="98"/>
+        <source>0 x 0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/division/image_division_gui.ui" line="107"/>
+        <source>Division</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/division/image_division_gui.ui" line="125"/>
+        <source>Horizontal:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/division/image_division_gui.ui" line="142"/>
+        <source>Vertical:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/division/image_division_gui.ui" line="161"/>
+        <source>Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/division/image_division_gui.ui" line="173"/>
+        <source>Width:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/division/image_division_gui.ui" line="193"/>
+        <source>Height:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/division/image_division_gui.ui" line="215"/>
+        <source>Ignore remainders</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/division/image_division_gui.cpp" line="72"/>
+        <source>%1 x %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ImageOpenDialog</name>
+    <message>
+        <location filename="../gui/image/basic/file_dialogs.cpp" line="26"/>
+        <source>Open Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ImageResizeGUI</name>
+    <message>
+        <location filename="../gui/image/resize/image_resize_gui.ui" line="74"/>
+        <source>Width:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/resize/image_resize_gui.ui" line="106"/>
+        <source>Height:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/resize/image_resize_gui.ui" line="135"/>
+        <source>Horizontal Scale:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/resize/image_resize_gui.ui" line="142"/>
+        <source>Vertical Scale:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/resize/image_resize_gui.ui" line="189"/>
+        <source>Keep aspect ratio</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/resize/image_resize_gui.ui" line="196"/>
+        <source>Smooth scaling</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ImageRotationGUI</name>
+    <message>
+        <location filename="../gui/image/rotation/image_rotation_gui.ui" line="86"/>
+        <source>Rotate clockwise by 90°</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/rotation/image_rotation_gui.ui" line="92"/>
+        <source>Rotate Clockwise</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/rotation/image_rotation_gui.ui" line="108"/>
+        <source>Rotate anti-clockwise by 90°</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/rotation/image_rotation_gui.ui" line="114"/>
+        <source>Rotate Anti-clockwise</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/rotation/image_rotation_gui.ui" line="130"/>
+        <source>Flip holizontally</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/rotation/image_rotation_gui.ui" line="136"/>
+        <source>Flip Horizontal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/rotation/image_rotation_gui.ui" line="152"/>
+        <source>Flip vertically</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/rotation/image_rotation_gui.ui" line="158"/>
+        <source>Flip Vertical</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ImageSaveDialog</name>
+    <message>
+        <location filename="../gui/image/basic/file_dialogs.cpp" line="41"/>
+        <source>Save Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ImageTransparentGUI</name>
+    <message>
+        <location filename="../gui/image/transparent/image_transparent_gui.ui" line="98"/>
+        <source>Color mode:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/transparent/image_transparent_gui.ui" line="133"/>
+        <source>Color:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/transparent/image_transparent_gui.ui" line="156"/>
+        <source>Tolerance:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/transparent/image_transparent_gui.ui" line="182"/>
+        <source>Transparency:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/image/transparent/image_transparent_gui.ui" line="210"/>
+        <source>Only contiguous area</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../gui/main_window.ui" line="182"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/main_window.ui" line="190"/>
+        <source>About DevTools</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QCoreApplication</name>
+    <message>
+        <location filename="../gui/image/basic/file_dialogs.cpp" line="15"/>
+        <source>Images</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Sidemenu</name>
+    <message>
+        <location filename="../gui/sidemenu.ui" line="53"/>
+        <source>Home</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/sidemenu.ui" line="81"/>
+        <source>Search tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Tool</name>
+    <message>
+        <location filename="../core/tool/tool.cpp" line="34"/>
+        <source>Image Resize</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../core/tool/tool.cpp" line="35"/>
+        <source>Image resizing and scaling</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../core/tool/tool.cpp" line="39"/>
+        <source>Image Rotation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../core/tool/tool.cpp" line="40"/>
+        <source>Image rotation and flipping</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../core/tool/tool.cpp" line="44"/>
+        <source>Image Division</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../core/tool/tool.cpp" line="45"/>
+        <source>Divide image into n x m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../core/tool/tool.cpp" line="49"/>
+        <source>Image Transparent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../core/tool/tool.cpp" line="50"/>
+        <source>Make the selected color transparent</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
