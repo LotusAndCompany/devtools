0ff89b4510941dba63dd0e05844ec7c681cba589
yusuke_yokota <yusuke@lotusandcompanyinc.com>
2025-01-16 11:49:41 +0900

    Fix translation file

--- res/dev-tools_en.ts
+++ res/dev-tools_en.ts
@@ -97,7 +97,7 @@
     </message>
     <message>
         <location filename="../gui/image/basic/control.ui" line="34"/>
-        <location filename="../gui/image/basic/control.cpp" line="63"/>
+        <location filename="../gui/image/basic/control.cpp" line="60"/>
         <source>Save</source>
         <translation type="unfinished"></translation>
     </message>
