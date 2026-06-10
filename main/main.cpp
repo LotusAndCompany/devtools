9f25d8450cbf5ae6d12ac2e14b961a396cdc076d
syunsuke-mitani-lotus <m.syunsuke@lotusandcompanyinc.com>
2026-06-10 21:47:01 +0900

    feat(logging): add colored formatted console output
    Install custom Qt message handler with:
    - Timestamps in [HH:MM:SS.zzz] format
    - File:line source location
    - Severity labels (DEBUG/INFO/WARN/CRIT/FATAL)
    - ANSI color coding (dim for debug, yellow for warning, red for critical)
    
    Fixes #12

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,8 @@
 #include "gui/gui_application.h"
 
+#include <QDateTime>
+#include <QFileInfo>
+
 #if defined(BUILD_TYPE_Debug) || defined(BUILD_TYPE_RelWithDebInfo)
 #include <iostream>
 __attribute__((destructor)) void destructor()
@@ -17,8 +20,65 @@ __attribute__((destructor)) void destructor()
 }
 #endif
 
+namespace {
+constexpr const char *ANSI_RESET = "\033[0m";
+constexpr const char *ANSI_DIM = "\033[2m";
+constexpr const char *ANSI_YELLOW = "\033[33m";
+constexpr const char *ANSI_RED = "\033[31m";
+
+const char *typeLabel(QtMsgType type)
+{
+    switch (type) {
+    case QtDebugMsg:
+        return "DEBUG";
+    case QtInfoMsg:
+        return "INFO ";
+    case QtWarningMsg:
+        return "WARN ";
+    case QtCriticalMsg:
+        return "CRIT ";
+    case QtFatalMsg:
+        return "FATAL";
+    }
+    return "?????";
+}
+
+const char *typeColor(QtMsgType type)
+{
+    switch (type) {
+    case QtDebugMsg:
+        return ANSI_DIM;
+    case QtInfoMsg:
+        return ANSI_RESET;
+    case QtWarningMsg:
+        return ANSI_YELLOW;
+    case QtCriticalMsg:
+    case QtFatalMsg:
+        return ANSI_RED;
+    }
+    return ANSI_RESET;
+}
+
+void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    const QString time = QDateTime::currentDateTime().toString("hh:mm:ss.zzz");
+    const QFileInfo file(context.file ? context.file : "");
+    const QString location =
+        context.file ? QString("%1:%2").arg(file.fileName()).arg(context.line) : QString();
+    fprintf(stderr, "%s[%s] %s%s%s %s\n", typeColor(type), qPrintable(time),
+            qPrintable(location.isEmpty() ? QString() : location + " "), typeLabel(type),
+            ANSI_RESET, qPrintable(msg));
+
+    if (type == QtFatalMsg) {
+        abort();
+    }
+}
+} // namespace
+
 int main(int argc, char *argv[])
 {
+    qInstallMessageHandler(messageHandler);
+
     GuiApplication app(argc, argv);
     app.setup();
 
